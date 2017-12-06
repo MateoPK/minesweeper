e9f375c706225bf714511f52dd73dda6b72f65bd
MateoPK <bathmaine23@yahoo.com>
2017-12-06 17:19:14 -0500

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,10 @@
+const blankLine = ' | | '
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
+console.log('This is what a board with a guess and a bomb looks like');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
