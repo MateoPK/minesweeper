22b3e6a5b279bf97e9a26cf445cfd7e5c7132b01
MateoPK <bathmaine23@yahoo.com>
2017-12-07 17:34:25 -0500

    next version

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,17 @@
-const blankLine = ' | | '
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-const guessLine = '1| | ';
-const bombLine = ' |B| ';
-console.log('This is what a board with a guess and a bomb looks like');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const board = [
+  [' ', ' ', ' ',],
+  [' ', ' ', ' ',],
+  [' ', ' ', ' ',]
+];
+
+
+const printBoard = () => {
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard();
