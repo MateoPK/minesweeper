4cd099aa598c1cd94c64205dcb7f5668fdc19059
MateoPK <bathmaine23@yahoo.com>
2017-12-13 22:27:58 -0500

    note about comit

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,40 +1,76 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  let board = [];
+  let board = [ ];
   for (let i = 0; i < numberOfRows; i++) {
-    let row = []
+    let row = [ ]
     for (let j = 0; j < numberOfColumns; j++) {
-          row.push(' ');
+          row.push( ' ' );
     }  board.push(row);
   }  return board;
 };
 
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-  let board = [];
+  let board = [ ];
   for (let i = 0; i < numberOfRows; i++) {
-    let row = []
+    let row = [ ]
     for (let j = 0; j < numberOfColumns; j++) {
           row.push(null);
     }  board.push(row);
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [[-1,-1][-1,0][-1,1][0,-1][1,1][0,1][1,-1][1,0]]
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  numberOfBombs = 0
+
+};
+
+
+
+
+
+
+
+
+
+
 
 };
 let numberOfBombsPlaced = 0;
   while (numberOfBombs > numberOfBombsPlaced) {
     let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    if (board[randomRowIndex][randomColumnIndex] !== 'b') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
     let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
     // An important note: The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
-    board[randomRowIndex][randomColumnIndex] = 'B';
+    board[randomRowIndex][randomColumnIndex] = 'b';
 
     numberOfBombsPlaced++;
   }
   return board;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const printBoard = (board) => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
-const playerBoard = generatePlayerBoard(3, 3);
-const bombBoard = generateBombBoard(3, 3, 4);
+const playerBoard = generatePlayerBoard(20, 20);
+const bombBoard = generateBombBoard(20, 20, 21);
 
 
 printBoard(playerBoard);
